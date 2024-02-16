79e07f1db021fc6341500efa982b3ef40cab200a
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-02-16 16:02:40 +0000

    player movement functions and boundaries

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,8 @@ const width = 15;
 const cellCount = width * width + 1;
 const cells = [];
 let playerCurrentPosition = 215;
+const playerBoundaryLeft = 208;
+const playerBoundaryRight = 223;
 
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
@@ -20,5 +22,24 @@ function addPlayer(position) {
   cells[position].classList.add("player");
 }
 
+function removePlayer(position) {
+  cells[position].classList.remove("player");
+}
+
 createGrid();
-addPlayer(playerCurrentPosition);
+
+function handleKeyDown(event) {
+  removePlayer(playerCurrentPosition);
+  if (event.keyCode === 37 && playerCurrentPosition !== playerBoundaryLeft) {
+    playerCurrentPosition--;
+  } else if (
+    event.keyCode === 39 &&
+    playerCurrentPosition !== playerBoundaryRight
+  ) {
+    playerCurrentPosition++;
+  }
+  addPlayer(playerCurrentPosition);
+  console.log(`player current position ${playerCurrentPosition}`);
+}
+
+document.addEventListener("keydown", handleKeyDown);
