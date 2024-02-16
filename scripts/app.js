01c1f67c10908e265f58b273a2b8c311ebb9ccea
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-02-16 15:16:28 +0000

    First commit, built basic html & css & injected a player class into grid cell

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
 console.log("Hello world!");
+
+const grid = document.querySelector(".grid");
+const width = 15;
+const cellCount = width * width + 1;
+const cells = [];
+let playerCurrentPosition = 215;
+
+function createGrid() {
+  for (let i = 0; i < cellCount; i++) {
+    const cell = document.createElement("div");
+    cell.innerText = i;
+    grid.appendChild(cell);
+    cells.push(cell);
+  }
+  addPlayer(playerCurrentPosition);
+}
+
+function addPlayer(position) {
+  cells[position].classList.add("player");
+}
+
+createGrid();
+addPlayer(playerCurrentPosition);
