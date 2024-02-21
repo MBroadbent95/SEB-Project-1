7329bb39b33c8c6224371ef206054c38a3ed9d8c
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-02-21 17:13:09 +0000

    graphic stylings, more tomorrow

--- scripts/app.js
+++ scripts/app.js
@@ -62,7 +62,7 @@ function removeAlien() {
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement("div");
-    cell.innerText = i;
+    //cell.innerText = i;
     grid.appendChild(cell);
     cells.push(cell);
   }
