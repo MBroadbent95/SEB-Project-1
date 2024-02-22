13d5e7319640140e9097e7ca36a1826971744175
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-02-22 15:55:43 +0000

    finalised sound

--- scripts/app.js
+++ scripts/app.js
@@ -15,17 +15,22 @@ const resetButton = document.getElementById("reset");
 const startButton = document.getElementById("start");
 const muteButton = document.getElementById("mute");
 let invasionBegins = null;
-let gameSpeed = 1000;
+let gameSpeed = 900; //-------------------------------------------------------------------------------------------
 let isPlaying = false;
 let direction = "RIGHT";
 let killCount = 0;
-//const nextURL = "https://my-space-invaders.com";
 let shotSound = new Audio("styles/sounds/HitMarker.wav");
 shotSound.volume = 0.3;
 let winSound = new Audio("styles/sounds/captureWin.wav");
 winSound.volume = 0.4;
 let backgroundTrack = new Audio("styles/sounds/crystal.wav");
 backgroundTrack.volume = 0.1;
+let gameOverSound = new Audio("styles/sounds/gameOver.wav");
+gameOverSound.volume = 0.3;
+let gotDamaged = new Audio("styles/sounds/damaged.wav");
+gotDamaged.volume = 0.4;
+let splat = new Audio("styles/sounds/splat.wav");
+splat.volume = 0.4;
 
 function reset() {
   isPlaying = false;
@@ -70,35 +75,24 @@ function removeAlien() {
 function createGrid() {
   for (let i = 0; i < cellCount; i++) {
     const cell = document.createElement("div");
-    //cell.innerText = i;
     grid.appendChild(cell);
     cells.push(cell);
   }
-  //addPlayer(playerCurrentPosition);
-  //addOneAlien(alienCurrentPosition);
-  //drawAliens();
 }
 
 function addPlayer(position) {
   cells[position].classList.add("player");
 }
 
-// function addOneAlien(position) {
-//   cells[position].classList.add("alien");
-// }
-
 function removePlayer(position) {
   cells[position].classList.remove("player");
 }
-// function removeOneAlien(position) {
-//   cells[position].classList.remove("alien");
-// }
+
 function removeShot(position) {
   cells[position].classList.remove("shot");
 }
 function addShot(position) {
   cells[position].classList.add("shot");
-  // shotSound.play();
 }
 function removeBomb(position) {
   cells[position].classList.remove("bomb");
@@ -106,11 +100,7 @@ function removeBomb(position) {
 function addBomb(position) {
   cells[position].classList.add("bomb");
 }
-//---------------------------------------------------------------------------
-// function fullClear() {
-//   cells.forEach.classList.remove("bomb", "shot");
-// }
-//---------------------------------------------------------------------------
+
 function aMoveRight() {
   removeAlien();
   alienPosition = alienPosition + 1;
@@ -142,10 +132,8 @@ function handleKeyDown(event) {
       playerCurrentPosition++;
     } else if (event.keyCode === 32) {
       fireShot();
-      //console.log("A shot was fired!");
     }
     addPlayer(playerCurrentPosition);
-    //console.log(`player current position ${playerCurrentPosition}`);
   }
 }
 
@@ -155,9 +143,7 @@ function fireShot() {
     removeShot(shotStart);
     shotStart = shotStart - 16;
     addShot(shotStart);
-    //console.log(shotStart);
     if (shotStart < 16) {
-      //console.log("the shot has stopped");
       removeShot(shotStart);
       clearInterval(shotInterval);
     }
@@ -169,22 +155,12 @@ function fireShot() {
       cells[hitByShot + alienPosition].classList.remove("alien");
       removeShot(shotStart);
       alienArmy = alienArmy.filter((alien) => alien !== hitByShot);
-      playerScore += 100;
+      playerScore += 200;
       killCount++;
       shotSound.play();
-      //console.log(`the killcount is ${killCount}`);
-      //console.log(`player score is ${playerScore}`);
       scoreDisplay.textContent = playerScore;
       clearInterval(shotInterval);
     }
-    // if (shotStart === alienPosition) {
-    //   removeShot(shotStart);
-
-    //   //removeAlien(alienPosition);
-    //   ///score
-    //   clearInterval(shotInterval);
-    //   console.log("the alien was destroyed");
-    // }
     if (isPlaying !== true) {
       removeShot(shotStart);
       clearInterval(shotInterval);
@@ -197,34 +173,27 @@ function randomAlien() {
 }
 
 function dropBomb() {
-  //if (isPlaying === true) {
   let bombStart = randomAlien() + alienPosition;
   const dropInterval = setInterval(() => {
     removeBomb(bombStart);
     bombStart = bombStart + 16;
     addBomb(bombStart);
-    //console.log("bombs away");
     if (bombStart > 207) {
-      //console.log("no more bomb");
       removeBomb(bombStart);
       clearInterval(dropInterval);
     }
     if (bombStart === playerCurrentPosition) {
+      gotDamaged.play();
       removeBomb(bombStart);
       clearInterval(dropInterval);
-      //console.log("player got dunked!");
+      playerScore -= 50;
       lives--;
     }
     if (isPlaying !== true) {
       removeBomb(bombStart);
       clearInterval(dropInterval);
     }
-  }, 500);
-  //}
-  // if (isPlaying !== true) {
-  //   removeBomb(bombStart);
-  //   clearInterval(dropInterval);
-  // }
+  }, 400);
 }
 
 function moveAliens() {
@@ -232,47 +201,26 @@ function moveAliens() {
     if (alienPosition % 16 === 5) {
       aMoveDown();
       direction = "LEFT";
-      //console.log("movedown 1 is called");
     } else {
       aMoveRight();
-      //console.log("moveright is called");
     }
   } else if (direction === "LEFT") {
     if (alienPosition % 16 === 0) {
       aMoveDown();
       direction = "RIGHT";
-      //console.log("movedown 2 is called");
     } else {
       aMoveLeft();
-      //console.log("moveleft is called");
     }
   }
 }
 
 function endGame() {
   console.log("Endgame activates");
-  //clearInterval(invasionBegins);
   removePlayer(playerCurrentPosition);
   removeAlien();
   isPlaying = false;
   backgroundTrack.pause();
-  winSound.play();
-  //console.log(`are you playing ${isPlaying}`);
-
-  //removeBomb(); ///the buck stops here for some reason
-  //fullClear();
-  //reset();
-  // killCount = 0;
-  // //clearInterval(dropInterval);
-  // alienArmy = [
-  //   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-  //   26, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 48, 49, 50, 51, 52, 53, 54,
-  //   55, 56, 57, 58,
-  // ];
-  // alienPosition = 18;
-
-  //console.log(`are you still playing ${isPlaying}`);
-  setTimeout(() => alert(`Amazing your score was ${playerScore}!`), 50);
+  setTimeout(() => alert(`Amazing your score was ${playerScore}!`), 100);
   const highScore = localStorage.removeItem("high-score");
   if (!highScore || playerScore > highScore) {
     localStorage.setItem("high-score", playerScore);
@@ -286,26 +234,12 @@ function invaded() {
         relativePosition + alienPosition === playerCurrentPosition
     )
   ) {
-    //console.log("alien collision");
     clearInterval(invasionBegins);
+    splat.play();
+    playerScore -= 200;
     endGame();
   }
 }
-
-// function victory() {
-//   console.log("Victory!");
-//   /////increase score
-//   //// reset game
-//   gameSpeed -= 500;
-//   ////begin game
-//   playerScore += 1000;
-//   scoreDisplay.textContent = playerScore;
-//   lives = 3;
-//   livesDisplay.innerHTML = "❤".repeat(lives);
-//   // isPlaying = false;
-//   clearInterval(invasionBegins);
-//   startGame();
-// }
 
 function startGame() {
   if (!isPlaying) {
@@ -321,17 +255,14 @@ function startGame() {
       invaded();
       if (!lives) {
         clearInterval(invasionBegins);
+        gameOverSound.play();
         endGame();
       }
       if (killCount === 44) {
-        console.log("You've defeated all of the aliens");
         clearInterval(invasionBegins);
+        winSound.play();
         endGame();
       }
-      // if ((alienArmy.length === 0)) {
-      //   console.log("Victory!");
-      //   //   victory();
-      // }
       livesDisplay.innerHTML = lives ? "❤".repeat(lives) : "☠";
     }, gameSpeed);
   }
@@ -342,6 +273,3 @@ document.addEventListener("keydown", handleKeyDown);
 startButton.addEventListener("click", startGame);
 resetButton.addEventListener("click", reset);
 muteButton.addEventListener("click", mute);
-//window.location.href = nextURL;
-//window.location.assign(nextURL);
-//window.location.replace(nextURL);
